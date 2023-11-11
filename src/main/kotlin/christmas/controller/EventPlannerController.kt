380859a115ebdfe114814743e0203710a489adcb
LAPTOP-P3BPH6JG\yoop1 <yoop1000@naver.com>
2023-11-11 18:31:00 +0900

    refactor : 방문 날짜 Int형으로 반환

--- src/main/kotlin/christmas/controller/EventPlannerController.kt
+++ src/main/kotlin/christmas/controller/EventPlannerController.kt
@@ -4,11 +4,11 @@ import christmas.view.InputView
 import christmas.view.OutputView
 
 class EventPlannerController(
-    private val input : InputView,
-    private val output : OutputView
+    private val input: InputView,
+    private val output: OutputView
 ) {
 
-    fun present(){
+    fun present() {
         output.printStart()
         input.readVisitDate()
     }
--- src/main/kotlin/christmas/view/InputView.kt
+++ src/main/kotlin/christmas/view/InputView.kt
@@ -6,19 +6,19 @@ import christmas.util.Validation.validateVisitDate
 
 class InputView {
 
-    fun readVisitDate(): String {
+    fun readVisitDate(): Int {
         println(REQUEST_VISIT_DATE)
 
         return checkVisitDate()
     }
 
-    private fun checkVisitDate() : String{
+    private fun checkVisitDate(): Int {
 
         return try {
             val input = Console.readLine()
             validateVisitDate(input)
-            input
-        } catch (e: IllegalArgumentException){
+            input.toInt()
+        } catch (e: IllegalArgumentException) {
             println(e)
             checkVisitDate()
         }
